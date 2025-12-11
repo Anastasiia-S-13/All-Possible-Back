a8972f896170b9ae046d7a5395077c29f22a61ad
Marian Narozhniak <narozhniakmarian@gmail.com>
2025-12-11 13:10:25 +0100

    rout_comtroller_validation

---
+++ src/routes/bookingRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { celebrate } from 'celebrate';
+import { checkAvailability, createBooking, getAllBookings } from '../controllers/bookingController.js';
+import { createBookingSchema } from '../validations/bookingValidations';
+
+
+
+const router = express.Router();
+
+router.get('/tools/:toolId/availability', checkAvailability);
+
+router.post('/bookings', `auth`, celebrate(createBookingSchema), createBooking);
+
+router.get('/bookings', getAllBookings); //! тимчасовий ендпоінт
