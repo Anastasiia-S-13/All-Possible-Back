d2f426751929e4b44f3a1fded280d7b7325273df
Yaroslav Hoian <sgoyan1405@gmail.com>
2025-12-11 02:17:12 +0200

    create route, validation, model and controller for feedbacks

---
+++ src/models/feedback.js
@@ -0,0 +1,29 @@
+import { model, Schema } from 'mongoose';
+
+const feedbackSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: false,
+      trim: true,
+      default: '',
+    },
+    rate: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+export const Feedback = model('Feedback', feedbackSchema);
