f9bd4394cefec3118c0bc03db85a31d12136014d
OlexandrVakulin <vlipoq@gmail.com>
2025-12-11 16:20:29 +0100

    add get categories api

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 import express from 'express';
-import cors from 'cors';
-import pino from 'pino-http';
+// import cors from 'cors';
+// import pino from 'pino-http';
 import 'dotenv/config';
+import { errors } from 'celebrate';
+import { notFoundHandler } from '../src/middleware/notFoundHandler.js';
+import { errorHandler } from '../src/middleware/errorHandler.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import categoriesRoutes from './routes/categoriesRoutes.js';
 
 const app = express();
 
 const PORT = process.env.PORT ?? 3000;
 
-
-
+app.use(categoriesRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
